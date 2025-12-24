bc17dc08a63b84f26ae1c02f0bc6ac1098e443cd
LUJIN-0 <lujainomar090@gmail.com>
2025-12-24 23:40:46 +0300

    .

--- src/config/awsConfig.js
+++ src/config/awsConfig.js
@@ -2,7 +2,6 @@
 
 import { Amplify } from 'aws-amplify';
 
-// Prevent double configure
 let configured = false;
 
 export function configureAmplify() {
@@ -14,9 +13,7 @@ export function configureAmplify() {
       Cognito: {
         userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID,
         userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID,
-        // State which identifiers are allowed at sign-in.
         loginWith: {
-          // username: true,
           email: true,
         },
       },
